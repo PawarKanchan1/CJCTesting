87ca3478b1c98030e104f7604b616eec21caeb41
PawarKanchan1 <123442477+PawarKanchan1@users.noreply.github.com>
2023-01-25 12:00:36 +0530

    By xyz

--- GitProject/src/com/Demo.java
+++ GitProject/src/com/Demo.java
@@ -2,6 +2,11 @@ package com;
 
 public class Demo
 {
+	public void m1()
+	{
+		System.out.println("this method m1");
+	}
+		
 	public static void main(String args[])
 	{
 		System.out.println("Hello");
